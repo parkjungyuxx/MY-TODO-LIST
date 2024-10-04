2937ef0d4cc18e1f962aaa88ecf4793985c14870
Jungyu Park <parkjungyu09@gmail.com>
2024-10-04 18:21:41 +0900

    feat: add delete all button, transform scale for all the buttons

--- js/todo.js
+++ js/todo.js
@@ -1,6 +1,7 @@
 const toDoForm = document.getElementById("todo-form");
 const toDoInput = toDoForm.querySelector("input");
 const toDoList = document.getElementById("todo-list");
+const deleteAllTheList = document.getElementById("deleteAllTheList");
 
 let toDos = [];
 const TODOS_KEY = "todos";
@@ -50,6 +51,12 @@ if (savedToDos !== null) {
   parsedToDos.forEach(paintToDo);
 }
 
+function deleteAllToDo(event) {
+  const liElements = document.querySelectorAll("li"); // 모든 li 요소 선택
+  liElements.forEach((li) => li.remove()); // 각 li 요소를 하나씩 삭제
+  toDos = []; // 배열도 초기화
+  saveToDos(); // localStorage도 초기화
+}
+
 toDoForm.addEventListener("submit", handleToDoSubmit);
-
-
+deleteAllTheList.addEventListener("click", deleteAllToDo);
